6fa348a81732aac34f4c037740e5f217f9c13307
Letícia Alves <leticiadeoliveira.corp@gmail.com>
2023-10-22 19:49:42 -0300

    comentários

--- src/index.js
+++ src/index.js
@@ -6,19 +6,6 @@ const express = require("express");
 const app = express(); //app será um objeto de servidor que será usado para fazer várias coisas como ligar o servidor.
 const axios = require("axios").create({baseUrl: "https://jsonplaceholder.typicode.com/"});
 
-
-app.get('/getAll/', function(req, res) {
-    //var cidade = req.body.cidade;
-    //console.log(cidade);
-    axios.get('https://api.openweathermap.org/data/2.5/weather?q=votuporanga&appid=12d006d950c398772ce30392456a62ad&units=metric')
-    .then(function (response) {
-       res.send(response.data);
-    })
-    .catch(function (error) {
-       res.status(500).send(error);
-    })
-});
-
 app.get('/getCity/:cidade', function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET'); 
@@ -34,9 +21,22 @@ app.get('/getCity/:cidade', function(req, res) {
   })
 });
 
-  app.get('/', (request, response) => {
+/* app.get('/getAll/', function(req, res) {
+    //var cidade = req.body.cidade;
+    //console.log(cidade);
+    axios.get('https://api.openweathermap.org/data/2.5/weather?q=votuporanga&appid=12d006d950c398772ce30392456a62ad&units=metric')
+    .then(function (response) {
+       res.send(response.data);
+    })
+    .catch(function (error) {
+       res.status(500).send(error);
+    })
+}); */
+
+
+/*   app.get('/', (request, response) => {
     return response.json({message: 'Hello World'});
-  })
+  }) */
 
 const port = process.env.PORT || 3000; //Para acessar a aplicação pelo navegador, é necessário ouvir (listen) uma porta.
 app.listen(port, () => {
